2e5d1c3903d65e452dc23e6365db943315034ead
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-07-09 20:49:51 +0530

    layout: add layout of tetris::game class

--- src/game.cpp
+++ src/game.cpp
@@ -1,71 +1,53 @@
 #include "game.hpp"
-#include <memory>
+//#include "renderer.hpp"
 
-game::game (std::unique_ptr<matrix> mat, std::unique_ptr<tetrimino> tet,
-            std::unique_ptr<screen> src, int screen_height)
-    : m_screen_height (screen_height), m_matrix (std::move (mat)),
-      m_tetriminos (std::move (tet)), m_screen (std::move (src))
+static constexpr auto board_width = 10u;
+static constexpr auto board_height = 20u;
+// static constexpr auto s_initialFramesPerFallStep = 48u;
+// static constexpr auto s_deltaFramesPerFallStepPerLevel = 2;
+
+//static const tetrimino s_tetrominos[static_cast<int> (tetrimino::type::count)]; // init later
+
+game::game () : m_game_state (game::state::title_screen) {}
+
+auto
+game::init_game () const -> bool
 {
-  init_game ();
+  return true;
 }
 
 auto
-game::draw_scene () -> void
+game::reset () -> void
 {
-  draw_matrix ();
 
-  draw_tetrimino (m_curr_pos, m_curr_type, m_curr_rotation);
-  draw_tetrimino (m_next_pos, m_next_type, m_next_rotation);
 }
 
 auto
-game::create_new_tetrimino () -> void
+game::shutdown () -> void
 {
-  m_curr_type = m_next_type;
-  m_curr_rotation = m_next_rotation;
-  auto initial_pos
-      = m_tetriminos->get_initial_position (m_curr_type, m_curr_rotation);
-  m_curr_pos = point (initial_pos.m_x, (matrix_width / 2) + initial_pos.m_y);
 
-  // TODO: randomly generate new tetirmnos
-  m_next_type = 0;
-  m_next_rotation = 0;
 }
 
 auto
-game::init_game () -> void
+game::update () -> void
 {
+
 }
 
 auto
-game::draw_tetrimino (const point pos, const int type, const int rotation)
-    -> void
+game::draw (renderer &p_renderer) -> void
 {
-  color mino_color;
 
-  auto pixel_pos = point (m_matrix->get_pixel_pos (pos));
+}
 
-  for (auto i = 0; i < block_size; ++i)
-    {
-      for (auto j = 0; j < block_size; ++j)
-        {
-          switch (m_tetriminos->get_mino_type (type, rotation, point (i, j)))
-            {
-            case 1:
-              mino_color = color::green;
-              break;
-            case 2:
-              mino_color = color::blue;
-              break;
-            }
+auto
+game::generate_tetrimino () -> bool
+{
+  return true;
+}
 
-          if (m_tetriminos->get_mino_type (type, rotation, point (i, j))x)
-            {
-              auto p1 = point (pixel_pos.m_x + i * mino_size,
-                               pixel_pos.m_y + j * mino_size);
-              auto p2 = point (p1.m_x + mino_size - 1, p1.m_y + mino_size - 1);
-              m_screen->draw_rectangle (p1, p2, mino_color);
-            }
-        }
-    }
+auto
+game::summon_tetrimino_to_board () -> void
+{
+
 }
--- src/game.hpp
+++ src/game.hpp
@@ -1,91 +1,67 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "matrix.hpp"
-#include "point.hpp"
-#include "screen.hpp"
-#include "tetrimino.hpp"
+#include "utils.hpp"
+
+class renderer;
+
+struct tetrimino
+{
+  static constexpr auto block_num = 4u;
+  static constexpr auto rotation_num = 4u;
+
+  coords block_coords[block_num][rotation_num];
+
+  unsigned int color;
+  enum class type
+  {
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
+    count
+  };
+};
+
+
+struct GameInput
+{
+  bool m_start;
+  bool m_pause;
+  bool m_move_left;
+  bool m_move_right;
+  bool m_rotate_clockwise;
+  bool m_rotate_anticlockwise;
+  bool m_hard_drop;
+  bool m_soft_drop;
+};
 
 class game
 {
 public:
-  game (std::unique_ptr<matrix> mat, std::unique_ptr<tetrimino> tet,
-        std::unique_ptr<screen> src, int screen_height);
+  game ();
 
-  auto draw_scene () -> void;
-  auto create_new_tetrimino () -> void;
-
-  // getters
-  auto
-  get_curr_type () const
-  {
-    return m_curr_type;
-  }
-  auto
-  get_curr_pos () const
-  {
-    return m_curr_pos;
-  }
-  auto
-  get_curr_rotation () const
-  {
-    return m_curr_rotation;
-  }
+  auto init_game () const -> bool;
+  auto reset () -> void;
+  auto shutdown () -> void;
+  auto update () -> void;
+  auto draw (renderer &p_renderer) -> void;
 
 private:
-  int m_screen_height;
+  auto generate_tetrimino () -> bool;
+  auto summon_tetrimino_to_board () -> void;
 
-  int m_next_type;
-  point m_next_pos;
-  int m_next_rotation;
+  enum class state
+  {
+    title_screen,
+    playing,
+    game_over,
+  };
 
-  int m_curr_type;
-  point m_curr_pos;
-  int m_curr_rotation;
-
-  std::unique_ptr<matrix> m_matrix;
-  std::unique_ptr<tetrimino> m_tetriminos;
-  std::unique_ptr<screen> m_screen;
-
-  auto init_game () -> void;
-  auto draw_tetrimino (const point p, const int type, const int rotation)
-      -> void;
-  auto draw_matrix () -> void;
+  state m_game_state;
 };
 
 #endif /* GAME_H */
-
-void
-Game::DrawPiece (int pX, int pY, int pPiece, int pRotation)
-{
-  color mColor; // Color of the block
-
-  // Obtain the position in pixel in the screen of the block we want to draw
-  int mPixelsX = mBoard->GetXPosInPixels (pX);
-  int mPixelsY = mBoard->GetYPosInPixels (pY);
-
-  // Travel the matrix of blocks of the piece and draw the blocks that are
-  // filled
-  for (int i = 0; i < PIECE_BLOCKS; i++)
-    {
-      for (int j = 0; j < PIECE_BLOCKS; j++)
-        {
-          // Get the type of the block and draw it with the correct color
-          switch (mPieces->GetBlockType (pPiece, pRotation, j, i))
-            {
-            case 1:
-              mColor = GREEN;
-              break; // For each block of the piece except the pivot
-            case 2:
-              mColor = BLUE;
-              break; // For the pivot
-            }
-
-          if (mPieces->GetBlockType (pPiece, pRotation, j, i) != 0)
-            mIO->DrawRectangle (
-                mPixelsX + i * BLOCK_SIZE, mPixelsY + j * BLOCK_SIZE,
-                (mPixelsX + i * BLOCK_SIZE) + BLOCK_SIZE - 1,
-                (mPixelsY + j * BLOCK_SIZE) + BLOCK_SIZE - 1, mColor);
-        }
-    }
-}
