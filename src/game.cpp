24f8a7784158ed1c6ef8d9db6f5c2e76e8966074
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-07-11 20:22:57 +0530

    game.cpp: add tetromino_data

--- src/game.cpp
+++ src/game.cpp
@@ -3,12 +3,89 @@
 
 static constexpr auto board_width = 10u;
 static constexpr auto board_height = 20u;
-// static constexpr auto s_initialFramesPerFallStep = 48u;
-// static constexpr auto s_deltaFramesPerFallStepPerLevel = 2;
+static constexpr auto initial_frames_fall_step = 48u;
 
-//static const tetrimino s_tetrominos[static_cast<int> (tetrimino::type::count)]; // init later
+// rotation data (uses SRS)
+static const tetromino tetromino_data[static_cast<int> (tetromino_type::count)]
+    = {
+        // I
+        {
+            {
+                { { 0, 1 }, { 1, 1 }, { 2, 1 }, { 3, 1 } },
+                { { 2, 0 }, { 2, 1 }, { 2, 2 }, { 2, 3 } },
+                { { 0, 2 }, { 1, 2 }, { 2, 2 }, { 3, 2 } },
+                { { 1, 0 }, { 1, 1 }, { 1, 2 }, { 1, 3 } },
+            },
+            0x00ffffff,
+        },
+        // J
+        {
+            {
+                { { 0, 0 }, { 0, 1 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 2, 0 }, { 1, 1 }, { 1, 2 } },
+                { { 0, 1 }, { 1, 1 }, { 2, 1 }, { 2, 2 } },
+                { { 1, 0 }, { 1, 1 }, { 0, 2 }, { 1, 2 } },
+            },
+            0x0000ffff,
+        },
+        // L
+        {
+            {
+                { { 2, 0 }, { 0, 1 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 2 } },
+                { { 0, 1 }, { 1, 1 }, { 2, 1 }, { 0, 2 } },
+                { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 1, 2 } },
+            },
+            0xffaa00ff,
+        },
+        // O
+        {
+            {
+                { { 1, 0 }, { 2, 0 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 2, 0 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 2, 0 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 2, 0 }, { 1, 1 }, { 2, 1 } },
+            },
+            0xffff00ff,
+        },
+        // S
+        {
+            {
+                { { 1, 0 }, { 2, 0 }, { 0, 1 }, { 1, 1 } },
+                { { 1, 0 }, { 1, 1 }, { 2, 1 }, { 2, 2 } },
+                { { 1, 1 }, { 2, 1 }, { 0, 2 }, { 1, 2 } },
+                { { 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 2 } },
+            },
+            0x00ff00ff,
+        },
+        // T
+        {
+            {
+                { { 1, 0 }, { 0, 1 }, { 1, 1 }, { 2, 1 } },
+                { { 1, 0 }, { 1, 1 }, { 2, 1 }, { 1, 2 } },
+                { { 0, 1 }, { 1, 1 }, { 2, 1 }, { 1, 2 } },
+                { { 1, 0 }, { 0, 1 }, { 1, 1 }, { 1, 2 } },
+            },
+            0x9900ffff,
+        },
+        // Z
+        {
+            {
+                { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 2, 1 } },
+                { { 2, 0 }, { 1, 1 }, { 2, 1 }, { 1, 2 } },
+                { { 0, 1 }, { 1, 1 }, { 1, 2 }, { 2, 2 } },
+                { { 1, 0 }, { 0, 1 }, { 1, 1 }, { 0, 2 } },
+            },
+            0xff0000ff,
+        }
+      };
 
-game::game () : m_game_state (game::state::title_screen) {}
+// helpers
+game::game ()
+    : m_frames_until_fall (initial_frames_fall_step),
+      m_game_state (game::state::title_screen)
+{
+}
 
 auto
 game::init_game () const -> bool
