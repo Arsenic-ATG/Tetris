c5a731c9e84ca51e6d6a2341f68144e34ecfd740
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-10-07 15:15:23 -0400

    add algo pseudo-code for next block visual

---
+++ src/issue22-algo.cpp
@@ -0,0 +1,22 @@
+// pseudocode
+
+// --- proposal ---
+mem = rand() // init
+
+
+do ()
+	func(mem)
+	.
+	.
+	.
+	mem = rand() 
+	
+
+// --- current ---
+
+do ()
+	mem = rand()
+	func(mem)
+	.
+	.
+	.
